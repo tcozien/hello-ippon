4a5418adf2247db2374acafbb3ec23a94e6f6b16
tcozien <thomas.cozien@gmail.com>
2017-06-30 10:37:49 +0200

    Update HelloIppon.java

--- HelloIppon.java
+++ HelloIppon.java
@@ -2,6 +2,7 @@ public class HelloIppon
 {
 	public static void main (String [] args)
 	{
+		System.out.println("  tcozien");
 		System.out.println("  ___ ____  ____   ___  _   _        __ _          _                     _        _                   _  ");
 		System.out.println(" |_ _|  _ \\|  _ \\ / _ \\| \\ | |  _   / _(_)_ __ ___| |_    ___ ___  _ __ | |_ __ _(_)_ __   ___ _ __  | | ");
 		System.out.println("  | || |_) | |_) | | | |  \\| | (_) | |_| |  __/ __| __|  / __/ _ \\|  _ \\| __/ _` | |  _ \\ / _ \\  __| | | ");
